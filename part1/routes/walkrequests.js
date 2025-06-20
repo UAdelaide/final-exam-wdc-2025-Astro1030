0799c1afc13e67d62cbc31ded1902b2771e7d7f7
Astro1030 <y15063304415@gmail.com>
2025-06-20 03:23:14 +0000

    [automatic commit] 2025-06-20 03:23:13 UTC 180.150.112.54

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -11,6 +11,6 @@ router.get('/open', async (req, res) => {
             JOIN Dogs d ON wr.dog_id = d.dog_id
             JOIN User u ON d.owner_id = u.user_id
             WHERE wr.status = 'open'
-        `)
+        `);
     }
 })
