8de26e5c27d853877b588f959dcc325c1e419974
Astro1030 <y15063304415@gmail.com>
2025-06-20 02:23:15 +0000

    [automatic commit] 2025-06-20 02:23:15 UTC 180.150.112.54

--- part1/db.js
+++ part1/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'password123',
   database: 'DogWalkService'
 });
 
-module.exports
+module.exports = db;
