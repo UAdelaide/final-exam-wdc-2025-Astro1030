0ba8dcb5300f10fd48fe735e0d40ff95f104fa7d
Astro1030 <y15063304415@gmail.com>
2025-06-20 12:43:34 +0000

    [automatic commit] 2025-06-20 12:43:34 UTC 129.127.202.94

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -55,4 +55,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) return res.status(500).json({ error: 'Failed to logout' });
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logged out successfully' });
+  });
+});
+
+
 module.exports = router;
+
