bef448c00c157f50b75a64f6e9dcffd12883dd72
Astro1030 <y15063304415@gmail.com>
2025-06-20 03:21:49 +0000

    [automatic commit] 2025-06-20 03:21:48 UTC 180.150.112.54

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -8,7 +8,8 @@ router.get('/open', async (req, res) => {
         const [rows] = await db.query(`
             SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
-            JOIN Dogs d ON wr.
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN User u ON d.owner
         `)
     }
 })
