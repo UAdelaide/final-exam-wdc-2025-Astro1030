5277092b20e269341d1e8169f641f7af8b14272a
Astro1030 <y15063304415@gmail.com>
2025-06-20 03:30:08 +0000

    [automatic commit] 2025-06-20 03:30:07 UTC 180.150.112.54

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -2,3 +2,11 @@ var express = require('express');
 var router = express.Router();
 var db =require('../db');
 
+// {
+//     "walker_username": "bobwalker",
+//     "total_ratings": 2,
+//     "average_rating": 4.5,
+//     "completed_walks": 2
+//   }
+
+
