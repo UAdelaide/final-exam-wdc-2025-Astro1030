3c26db9a298d450c7e9611182572396cee83364d
Astro1030 <y15063304415@gmail.com>
2025-06-20 03:25:42 +0000

    [automatic commit] 2025-06-20 03:25:42 UTC 180.150.112.54

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -14,6 +14,7 @@ router.get('/open', async (req, res) => {
         `);
         res.json(rows);
     } catch(err) {
-        console.error('Error fetching open walk request:', err.message)
+        console.error('Error fetching open walk request:', err.message);
+        res.status(500).json({})
     }
 })
