12f77cce49661620e47aa4f16b506ba8512dae6b
Astro1030 <y15063304415@gmail.com>
2025-06-20 02:48:24 +0000

    [automatic commit] 2025-06-20 02:48:24 UTC 180.150.112.54

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//
 const dogsRouter = require('./routes/dogs');
 const walkrequestsRouter = require('./routes/walkrequests');
 const walkersRouter = require('./routes/walkers');
@@ -18,6 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//
+app.use('/api/dogs', dogsRouter);
+app.use('/api/walkrequests', walkrequestsRouter);
+app.use('/api/walkers', walkersRouter);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
