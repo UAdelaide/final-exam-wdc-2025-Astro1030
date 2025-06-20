1945fa9807d2e396f67243ad436612e26a12c37c
Astro1030 <y15063304415@gmail.com>
2025-06-20 13:39:39 +0000

    [automatic commit] 2025-06-20 13:39:39 UTC 129.127.202.94

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async (requestAnimationFrame.))
+router.get('/dogs', async (req, res) => {
+    try {
+        const[]
+    }
+})
