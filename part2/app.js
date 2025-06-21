2d2c693511a9dafe413b76658e7493a9086e5475
Astro1030 <y15063304415@gmail.com>
2025-06-21 02:32:30 +0000

    [automatic commit] 2025-06-21 02:32:29 UTC 180.150.112.54

--- part2/app.js
+++ part2/app.js
@@ -27,7 +27,6 @@ const dogsRouter = require('./routes/dogs');
 app.use('/api/walks', walkRoutes);
 app.use('/api', userRoutes);
 app.use('/api/dogs', dogsRouter);
-app.use('/api/user', userRoutes);
 
 
 
